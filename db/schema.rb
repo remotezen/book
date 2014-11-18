97851033908c924e39af44889784f028937f3143
Fred Hudson <remotezen@bell.net>
2014-11-18 09:46:00 -0500

    commit for github push

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 4) do
+ActiveRecord::Schema.define(version: 5) do
 
   create_table "jobs", force: true do |t|
     t.string  "title"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 4) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password"
+    t.string   "confirmation_code"
+    t.boolean  "confirmation",      default: false
   end
 
 end
